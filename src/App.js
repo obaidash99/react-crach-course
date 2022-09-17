90f749083dc954ff6065e7f88230c0917c6e4e49
obaida <obaidashurbaji99@gmail.com>
2022-09-17 20:33:55 +0200

    useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
 	const [counter, setCounter] = useState(0);
+
+	useEffect(() => {
+		setCounter(100);
+	}, []);
 
 	return (
 		<div className="App">
