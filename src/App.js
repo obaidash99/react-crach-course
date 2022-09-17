e0fb4d227c5e7628e71e815fd561e6a158768c82
obaida <obaidashurbaji99@gmail.com>
2022-09-17 20:25:09 +0200

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 
 const App = () => {
-	const Person = (props) => {
-		return (
-			<>
-				<h1>First Name: {props.name}</h1>
-				<h2>Last Name: {props.lastName}</h2>
-				<h2>Age: {props.age}</h2>
-			</>
-		);
-	};
+	const [counter, setCounter] = useState(0);
 
 	return (
 		<div className="App">
-			<h1>Hello, {2 + 2}!</h1>
-			<Person name={'Obaida'} lastName={'Mohammed'} age={23} />
-			<Person name={'Hany'} lastName="Shurbaji" age={26} />
+			<button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+			<h1>{counter}</h1>
+			<button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
 		</div>
 	);
 };
