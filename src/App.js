db0faa3e996eb2db3e4385176a7072d6a1774bda
obaida <obaidashurbaji99@gmail.com>
2022-09-17 20:11:24 +0200

    props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 
 const App = () => {
-	const Person = () => {
+	const Person = (props) => {
 		return (
 			<>
-				<h1>First Name: Obaida</h1>
-				<h2>Last Name: Shurbaji</h2>
-				<h2>Age: 23</h2>
+				<h1>First Name: {props.name}</h1>
+				<h2>Last Name: {props.lastName}</h2>
+				<h2>Age: {props.age}</h2>
 			</>
 		);
 	};
@@ -14,7 +14,8 @@ const App = () => {
 	return (
 		<div className="App">
 			<h1>Hello, {2 + 2}!</h1>
-			<Person />
+			<Person name={'Obaida'} lastName={'Mohammed'} age={23} />
+			<Person name={'Hany'} lastName="Shurbaji" age={26} />
 		</div>
 	);
 };
